1efbb95f1c98a6848fca6af4ef85c1b54820f773
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2015-04-10 18:00:17 +0300

    Apply beebox JSON path

--- client/app/src/main/java/team/monroe/org/meetpy/uc/GetTaskDetails.java
+++ client/app/src/main/java/team/monroe/org/meetpy/uc/GetTaskDetails.java
@@ -25,17 +25,13 @@ public class GetTaskDetails extends UserCaseSupport<TaskIdentifier, Task> {
             HttpManager.Response<Json> response = using(HttpManager.class).get(server.append("task", request.taskId), HttpManager.details(), HttpManager.response_json());
             String title = response.body.asObject().asString("title");
             String description = response.body.asObject().asString("description");
-            Object progressObj = response.body.asObject().value("progress", Object.class);
-            float progress = 1f;
-            if (progressObj instanceof Double){
-                progress = ((Double) progressObj).floatValue();
-            }
+            float progress = response.body.asObject().value("progress", Float.class);
             Task.Status status = Task.Status.unknown;
             try {
                 status = Task.Status.valueOf(response.body.asObject().asString("status"));
             }catch (Exception e){}
 
-            return new Task(request,title, description, status, (float) progress);
+            return new Task(request,title, description, status, progress);
         } catch (HttpManager.InvalidBodyFormatException e) {
             throw new FailExecutionException(e,100);
         } catch (IOException e) {
