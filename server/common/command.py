65a0d7f9e4d7a7804382ee7f3ed3806b644b3df0
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2015-05-03 16:15:53 +0300

    Remove str

--- server/common/command.py
+++ server/common/command.py
@@ -65,9 +65,9 @@ class ChoiceArgumentDefinition(_ArgumentDefinition):
 
     def choice(self, title, description, value):
         self._choices.append({
-            "title": str(title),
-            "description": str(description),
-            "value": str(value),
+            "title": title,
+            "description": description,
+            "value": value,
             })
         return self
 
